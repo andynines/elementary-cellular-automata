0a36b7e0e814a9be1c512b032ea9ad6879881bed
andynines <andrewsenin@comcast.net>
2018-08-22 22:48:01 -0400

    Added ECA header

---
+++ eca.h
@@ -0,0 +1,78 @@
+/*
+eca.h
+Copyright (c) 2018 andynines
+MIT License
+*/
+
+#ifndef ECA_H
+
+#define ECA_H
+
+
+
+// Includes
+
+#include <stdbool.h>
+
+
+
+// Data types
+
+typedef enum {
+    // Represent different configurations for generation initializer
+    ALL_DEAD,
+    SINGLE_CENTER_ALIVE,
+    SINGLE_RANDOM_ALIVE,
+    QUARTER_SPACED_ALIVE,
+    QUARTER_RANDOM_ALIVE,
+    HALF_SPACED_ALIVE,
+    HALF_RANDOM_ALIVE,
+    THREEQUARTERS_SPACED_ALIVE,
+    THREEQUARTERS_RANDOM_ALIVE,
+    SINGLE_CENTER_DEAD,
+    SINGLE_RANDOM_DEAD,
+    ALL_ALIVE
+} ConfigCode;
+
+typedef enum {
+    // Specifies how boundaries are treated
+    WRAP,
+    DEAD_BORDERS,
+    ALIVE_BORDERS
+} BoundaryCode;
+
+typedef unsigned short int CellBlock;
+static const int blockBytes = sizeof(CellBlock);
+static const int blockBits = blockBytes * 8;
+
+typedef struct {
+    CellBlock* blockArr;
+} Generation;
+
+typedef struct {
+    int age;
+    int rule;
+    int habitatSize;
+    int genBufferSize;
+    int blockReq;
+    BoundaryCode borderType;
+    Generation* genArr;
+} Simulation;
+
+
+
+// Function declarations
+
+void iterateSim(Simulation* simPtr, int iterations);
+
+Simulation* createSim(int rule,
+                      int habitatSize,
+                      int genBufferSize,
+                      BoundaryCode borderType,
+                      ConfigCode initCode);
+
+void destroySim(Simulation* simPtr);
+
+
+
+#endif
