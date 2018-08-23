fa3e9c81d3b3fe757e7ae13eef3812dc008ba8e1
andynines <andrewsenin@comcast.net>
2018-08-22 22:46:17 -0400

    Added utils header

---
+++ utils.h
@@ -0,0 +1,29 @@
+/*
+utils.h
+Copyright (c) 2018 andynines
+MIT License
+*/
+
+#ifndef UTILS_H
+
+#define UTILS_H
+
+
+
+// Includes
+
+#include <stdbool.h>
+
+
+
+// Function declarations
+
+bool wrap(int* n, int min, int max);
+
+bool binomProb(double p);
+
+int gcd(int a, int b);
+
+
+
+#endif
