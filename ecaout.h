d894d5f789eb5b95dc380ea74714537907d14266
andynines <andrewsenin@comcast.net>
2018-08-22 22:50:16 -0400

    Added ECA output header

---
+++ ecaout.h
@@ -0,0 +1,36 @@
+/*
+ecaout.h
+Copyright (c) 2018 andynines
+MIT License
+*/
+
+#ifndef ECAOUT_H
+
+#define ECAOUT_H
+
+
+
+// Includes
+
+#include "eca.h"
+
+
+
+// Available output characters
+
+typedef enum {
+    DEAD_CELL = '.',
+    ALIVE_CELL = '#'
+} OutChar;
+
+
+
+// Function declarations
+
+void genOut(Simulation* simPtr, int genIndex);
+
+void simOut(Simulation* simPtr);
+
+
+
+#endif
